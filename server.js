84fc7f428a7bdb69cb33f30e848b967c262d178a
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-02 10:34:28 -0400

    testing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
-friendsArray = require("./App/Data/friends.js");
+
+var friendsArray = require("./app/data/friends.js");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,10 +9,10 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static('App'))
+// app.use(express.static('App'))
 
-require("./App/Routing/apiRoutes")(app);
-require("./App/Routing/htmlRoutes")(app);
+require("./app/Routing/apiRoutes")(app);
+require("./app/Routing/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
